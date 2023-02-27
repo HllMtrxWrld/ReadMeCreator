252d452d42989676f8d8fd275f1c9dc317cea559
HllMtrxWrld <119111798+HllMtrxWrld@users.noreply.github.com>
2023-02-27 18:33:07 +0000

    Functions to  write README file and initialize program and were written

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ReadmeCreator
+# idk2
+
--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const fs = require("fs");
-const path = require('path');
+const fs = require('fs');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: 'title',
+        message: 'What is your projects title?',
+    }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(response) {
+    fs.writeFile('README.md', response, err => {
+        err ? console.error(err) : console.log("The README.md is now created!")
+    })
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then(generateMarkdown)
+    .then(writeToFile);
 }
 
 // function call to initialize program
