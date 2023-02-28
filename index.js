2fa7ac961afa61c4d19b6abb3881d57f0799c7f6
HllMtrxWrld <119111798+HllMtrxWrld@users.noreply.github.com>
2023-02-28 11:43:35 +0000

    questions were added

--- index.js
+++ index.js
@@ -7,8 +7,58 @@ const questions = [
     {
         type: "input",
         name: 'title',
-        message: 'What is your projects title?',
-    }
+        message: 'Whats the name of your project?',
+    },
+    {
+        type: "input",
+        name: 'description',
+        message: 'What is your project about?',
+    },
+    {
+        type: "input",
+        name: 'installation',
+        message: 'Provide installation guidelines:',
+    },
+    {
+        type: "input",
+        name: 'usage',
+        message: 'Give details on how your project will be used:',
+    },
+    {
+        type: "input",
+        name: 'contributing',
+        message: 'Provide information on the contribution guidelines for your project:',
+    },
+    {
+        type: "input",
+        name: 'tests',
+        message: 'Provide test instructions for your project:',
+    },
+    {
+        type: "input",
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: [
+            'MIT',
+            'Apache license 2.0',
+            'Boost Software License 1.0'
+        ]
+    },
+    {
+        type: "input",
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: "input",
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: "input",
+        name: 'questions',
+        message: 'Please provide instuctions on how to reach you with additional questions:',
+    },
 ];
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,29 @@
 function generateMarkdown(response) {
   return `# ${response.title}
 
+## Description
+${response.description}
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## Contributing
+${response.contributing}
+
+## Tests
+${response.tests}
+
+## License
+Licensed by ${response.license}
+
+## Questions
+${response.questions}
+* GitHub: [${response.username}](https://github.com/${response.username})
+* Email: ${response.email}
+
 `;
 }
 
